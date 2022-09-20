8e66456195c27eee8a7d6b58a55fcbefdeea8111
Rosangela <davilla.witte@gmail.com>
2022-09-19 22:53:08 -0300

    98. add pagination

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,3 +1,4 @@
+import { IType } from './../shared/models/productType';
 import { ShopService } from './shop.service';
 import { IProduct } from '../shared/models/product';
 import { Component, OnInit } from '@angular/core';
@@ -9,15 +10,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopComponent implements OnInit {
   products: IProduct[];
+  types: IType[];
+  typeIdSelected = 0; //seleciona automaticamente o Id 0 - Home atualiza com "todos" selecionado
+  sortSelected = 'name';
+  sortOptions = [
+    {name: 'Ordem Alfabética', value: 'name'},
+    {name: 'Preço: Menor para o Maior', value: 'priceAsc'},
+    {name: 'Preço: Maior para o Menor', value: 'priceDesc'}
+  ];
 
   constructor(private shopService: ShopService) { }
 
   ngOnInit(): void {
-    this.shopService.getProducts().subscribe(response => {
+    this.getProducts();
+    this.getTypes();
+  }
+
+  getProducts(){
+    this.shopService.getProducts(this.typeIdSelected, this.sortSelected).subscribe(response => {
       this.products = response.data;
     }, error => {
       console.log(error);
     });
   }
+
+  getTypes(){
+    this.shopService.getTypes().subscribe(response =>{
+      //Faz o filtro do Shop component por modelos de produtos
+      this.types = [{id:0, name:'Todos'}, ...response];
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  onTypeSelected(typeId: number){
+    this.typeIdSelected = typeId;
+    this.getProducts();
+  }
+
+
+  onSortSelected(sort: string){
+    this.sortSelected = sort;
+    this.getProducts();
+  }
+
+
+
 
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,6 +1,8 @@
 import { IPagination } from '../shared/models/pagination';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IType } from '../shared/models/productType';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +12,27 @@ export class ShopService {
 
   constructor(private http: HttpClient) { }
 
-  getProducts(){
-    return this.http.get<IPagination>(this.baseUrl + 'products?pageSize=50');
+  getProducts(typeId?:number, sort?: string){
+    let params = new HttpParams();
+
+    if (typeId){
+      params = params.append('typeId', typeId.toString());
+    }
+
+    if (sort){
+      params = params.append('sort', sort);
+    }
+    
+    return this.http.get<IPagination>(this.baseUrl + 'products', {observe: 'response', params})
+      .pipe(
+        map(response => {
+          return response.body;
+        })
+      );
   }
+
+  getTypes(){
+    return this.http.get<IType[]>(this.baseUrl + 'products/types');
+  }
+
 }
